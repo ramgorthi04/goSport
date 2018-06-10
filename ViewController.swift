9059482bb92cf25685f55ca4ca3eacea36d9ee53
Ram Gorthi <30302781+ramgorthi04@users.noreply.github.com>
2018-06-10 00:35:17 -0700

    Create ViewController.swift

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoSPORTProto
+//
+//  Created by Hackathon Event on 6/9/18.
+//  Copyright © 2018 Hackathon Event. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .Plain, target: self, action: #selector(handleLogout))
+    }
+    
+    func handleLogout() {
+        let loginController = LoginController()
+        presentViewController(loginController, animated: true, completion: (() -> Void)?)
+    }
+
+}
+
